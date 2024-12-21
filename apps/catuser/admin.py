4e83153f9b35afc3ac5a2855124e6fd34ac7f8c5
Wolfgang Almeida <wolfgang.almeida@inchurch.com.br>
2024-12-21 00:24:36 -0300

    Add CatUserAdmin class

--- apps/catuser/admin.py
+++ apps/catuser/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from apps.catuser.models import CatUser
+
 # Register your models here.
+# ==========================
+class CatUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+admin.site.register(CatUser, CatUserAdmin)
