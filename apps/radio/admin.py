c813c64cb96754bf90c70753768a03f577cc0b3d
Wolfgang Almeida <wolfgang.almeida@inchurch.com.br>
2024-12-22 20:43:37 -0300

    Add Radio models

--- apps/radio/admin.py
+++ apps/radio/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from apps.radio.models import Radio, RadioStream
+
+
 # Register your models here.
+# ==========================
+class RadioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'identifier', 'is_active', 'created_at', 'updated_at')
+    search_fields = ('name', 'identifier', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_active', 'created_at', 'updated_at')
+    ordering = ('name', 'created_at', 'updated_at')
+
+    class Meta:
+        model = Radio
+
+
+class RadioStreamAdmin(admin.ModelAdmin):
+    list_display = ('radio', 'identifier', 'title', 'is_active', 'created_at', 'updated_at')
+    search_fields = ('radio', 'identifier', 'title', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('radio', 'is_active', 'created_at', 'updated_at')
+    ordering = ('radio', 'title', 'is_active', 'created_at', 'updated_at')
+
+    class Meta:
+        model = RadioStream
+
+
+admin.site.register(Radio, RadioAdmin)
+admin.site.register(RadioStream, RadioStreamAdmin)
--- apps/radio/models.py
+++ apps/radio/models.py
@@ -1,3 +1,44 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+# ========================
+class Radio(models.Model):
+    name = models.CharField(max_length=255)
+    identifier = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    description = models.TextField(default=None, blank=True, null=True)
+    cover = models.FileField(upload_to='radio/cover/', default=None, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    # Properties
+    @property
+    def playing_now(self):
+        return self.radio_streams.filter(is_active=True).first()
+
+
+class RadioStream(models.Model):
+    radio = models.ForeignKey(Radio, on_delete=models.SET_NULL, null=True, blank=True, related_name="radio_streams")
+    audios = models.ManyToManyField('streaming.Audio', blank=True)
+    identifier = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    title = models.CharField(max_length=255, default=None, blank=True, null=True)
+
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "[{}] {}".format(self.radio.name, self.title)
+
+    # Properties
+    @property
+    def total_duration(self):
+        return sum([audio.duration_in_seconds for audio in self.audios.all()])
